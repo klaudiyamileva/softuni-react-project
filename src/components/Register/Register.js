11b8082a07e027c9125c14417d1abb637d9cdbcb
klaudiyamileva <klaudiyamileva@gmail.com>
2023-03-20 16:09:11 +0200

    added register validations

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,5 @@
 import styles from './Register.module.css';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 import * as authService from '../../services/authService';
@@ -8,39 +8,113 @@ export const Register = () => {
     const { userLogin } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const onSubmit = (e) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [formErrors, setFormErrors] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const validateForm = () => {
+        let valid = true;
+        const errors = {};
+
+        if (!formData.email) {
+            errors.email = 'Email is required';
+            valid = false;
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            errors.email = 'Invalid email format';
+            valid = false;
+        }
+
+        if (!formData.password) {
+            errors.password = 'Password is required';
+            valid = false;
+        } else if (formData.password.length < 5) {
+            errors.password = 'Password must be at least 5 characters long';
+            valid = false;
+        }
+
+        if (!formData.confirmPassword) {
+            errors.confirmPassword = 'Please confirm your password';
+            valid = false;
+        } else if (formData.password !== formData.confirmPassword) {
+            errors.confirmPassword = 'Passwords do not match';
+            valid = false;
+        }
+
+        setFormErrors(errors);
+
+        return valid;
+    };
+
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        const formData = new FormData(e.target);
-
-        const email = formData.get('email');
-        const password = formData.get('password');
-        const repass = formData.get('confirm-password');
-
-        if(password !== repass) {
-            return;
+        if (validateForm()) {
+            authService
+                .register(formData.email, formData.password)
+                .then((authData) => {
+                    userLogin(authData);
+                    navigate('/');
+                })
+                .catch(() => {
+                    navigate('/register');
+                });
         };
+    };
 
-        authService.register(email, password)
-            .then(authData => {
-                userLogin(authData);
-                navigate('/');
-            })
-            .catch(() => {
-                navigate('/');
-            });
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     return (
         <>
-            <form onSubmit={onSubmit} className={styles.col} id="contact">
-                <h4> Register </h4>
-                <input type="email" name="email" className={styles.form} placeholder="Email..." />
-                <input type="password" name="password" className={styles.form} placeholder="Password..." />
-                <input type="password" name="confirm-password" className={styles.form} placeholder="Repeat Password..." />
+            <form onSubmit={handleSubmit} className={styles.col} id="contact">
+                <h4>Register</h4>
+                <div>
+                    <input
+                        type="email"
+                        name="email"
+                        className={styles.form}
+                        placeholder="Email..."
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    {formErrors.email && <div className="error">{formErrors.email}</div>}
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        name="password"
+                        className={styles.form}
+                        placeholder="Password..."
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    {formErrors.password && <div className="error">{formErrors.password}</div>}
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        className={styles.form}
+                        placeholder="Repeat Password..."
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                    />
+                    {formErrors.confirmPassword && <div className="error">{formErrors.confirmPassword}</div>}
+                </div>
                 <input type="submit" className={styles.more} value="Register" />
             </form>
-            <p>If you don't have an account <Link to={'/login'}>click here</Link></p>
+            <p>
+                If you don't have an account <Link to={'/login'}>click here</Link>
+            </p>
         </>
     );
 };
