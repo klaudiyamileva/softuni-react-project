b602604704873164c25838ec276637976cc9623d
klaudiyamileva <klaudiyamileva@gmail.com>
2023-03-22 14:41:28 +0200

    added private routes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { Logout } from './components/Logout/Logout';
 import { Register } from './components/Register/Register';
 import { Work } from './components/Work/Work';
 import { AuthProvider } from './contexts/AuthContext';
+import { PrivateRoute } from './components/common/PrivateRoute';
 
 function App() {
     return (
@@ -26,14 +27,16 @@ function App() {
                         <Route path='/work' element={<Work />}></Route>
                         <Route path='/blog' element={<AllBlogs />}></Route>
                         <Route path='/blog/:blogId' element={<Blog />}></Route>
-                        <Route path='/blog/add' element={<BlogAdd />}></Route>
-                        <Route path='/blog/:blogId/edit' element={<BlogEdit />}></Route>
                         <Route path='/careers' element={<AllCareers />}></Route>
                         <Route path='/careers/:careerId' element={<Career />}></Route>
-                        <Route path='/contact' element={<Contact />}></Route>
                         <Route path='/login' element={<Login />}></Route>
                         <Route path='/register' element={<Register />}></Route>
-                        <Route path='/logout' element={<Logout />}></Route>
+                        <Route element={<PrivateRoute />}>
+                            <Route path='/blog/add' element={<BlogAdd />}></Route>
+                            <Route path='/blog/:blogId/edit' element={<BlogEdit />}></Route>
+                            <Route path='/contact' element={<Contact />}></Route>
+                            <Route path='/logout' element={<Logout />}></Route>
+                        </Route>
                     </Routes>
                 </main>
                 <Footer />
