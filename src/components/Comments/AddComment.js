53b073bd6d64e9e2d2094ca4063786de02749af6
klaudiyamileva <klaudiyamileva@gmail.com>
2023-03-28 11:34:10 +0300

    added AddComment component

--- src/components/Comments/AddComment.js
+++ src/components/Comments/AddComment.js
@@ -68,6 +68,5 @@ export const AddComment = ({
             {formErrors.comment && <div className="error">{formErrors.comment}</div>}
             <input type="submit" className={styles.more} value="SENT" />
         </form>
-
     )
 }
